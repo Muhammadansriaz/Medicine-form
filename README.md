ef85a90848bed8a9f627ba79afbafe666a59c4ec
Muhammad Ans <75665100+Muhammadansriaz@users.noreply.github.com>
2022-08-23 05:36:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Medicine-form
+ In this project we get the multiple input fileds values and checkbox values using a single use state and also render the value from a json file using map
---
+++ src/PastHistory.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const PastHistory = () => {
+  return (
+    <div><div className="mb-3">
+      <textarea
+        className="form-control mt-4 mx-2"
+        id="exampleFormControlTextarea1"
+        rows={3}
+        defaultValue={""}
+      />
+    </div>
+
+    </div>
+  )
+}
+
+export default PastHistory
